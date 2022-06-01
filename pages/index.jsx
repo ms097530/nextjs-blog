a593476d6ba4b9e460418a0999604ce439611422
Michael Schultz <mikewschultz9029@gmail.com>
2022-06-01 11:18:24 -0400

    added text to index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,7 +14,7 @@ export default function Home({ allPostsData })
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Yo yo yo</p>
+        <p>Yo yo yo. It's ya boiii. In the house. Or something...</p>
         <p>
           (This is a sample website - you'll be building a site like this on{' '}
           <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
